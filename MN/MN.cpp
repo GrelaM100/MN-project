3572fb047bbcf2bf85ee642ae8091760f4d76c33
acieslik8 <57452635+acieslik8@users.noreply.github.com>
2021-05-27 01:17:26 +0200

    Added functions create_example and split_row

--- MN/MN.cpp
+++ MN/MN.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream> 
+#include <string>
+#include <time.h>
 #include "interval.h"
 #include "hansen_arith.h"
 #include "hansen_intervals_parser.h"
@@ -29,8 +32,8 @@ void print(std::vector<hansen_interval> const& input)
 vector<interval> solve(system_of_linear_equations sole) {
     interval_matrix A = sole.A();
     interval_matrix b = sole.b();
-    cout << "A =\n" << A << "\n";
-    cout << "b = \n" << b << "\n";
+    //cout << "A =\n" << A << "\n";
+    //cout << "b = \n" << b << "\n";
     int n = A.get_rows();
     int m = A.get_cols();
     vector<interval> I(n);
@@ -48,6 +51,7 @@ vector<interval> solve(system_of_linear_equations sole) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             a = A(j, i) / A(i, i);
+            a = interval(-0.55, -0.45);
             for (int k = 0; k < n; k++) {
                 A(j, k) = A(j, k) - a * A(i, k);
             }
@@ -106,83 +110,106 @@ vector<hansen_interval> solve_using_hansen(system_of_linear_equations_hansen_for
     return(I);
 }
 
+vector<interval> split_row(string s) {
+    vector<double> list;
+    vector<interval> new_row;
+    string delimiter = ";";
 
-void run_example() { //kod testowy
-    cout << "Przykladowe rownanie:\n";
-    interval a1, a2, a3, a4, b1, b2;
-    std::vector <interval> intervals;
-    std::vector <hansen_interval> hansen_intervals;
+    size_t pos = 0;
+    string token;
+    while ((pos = s.find(delimiter)) != string::npos) {
+        token = s.substr(0, pos);
+        list.push_back(stoi(token));
+        s.erase(0, pos + delimiter.length());
+    }
+    list.push_back(stoi(s));
 
-    a1 = interval(1, 5);
-    a2 = interval(3, 9);
-    a3 = interval(2, 3);
-    a4 = interval(9, 10);
-    b1 = interval(1, 4);
-    b2 = interval(1, 10);
+    interval element;
+    for (int i = 0; i < list.size()/2; i++) {
+        element = interval(min(list[2*i], list[2*i+1]), max(list[2 * i], list[2 * i + 1]));
+        //cout << element << " ";
+        new_row.push_back(element);
+    }
+    return(new_row);
+}
+
+system_of_linear_equations create_example() {
+    int input;
+    cout << "Rozwiazywanie ukladu rownan metoda Gassa.\nWybierz co chcesz zrobic\n\t1. Wylosuj uklad rownan\n\t2. Wczytaj uklad rownan\n";
+    cin >> input;
+    vector<interval> rows;
+    vector<vector<interval>> matA;
+    vector<vector<interval>> matB;
+    interval element;
+    int n = rand() % 4 + 2;
+    int a1, a2;
+    string row_input;
 
 
-    intervals.push_back(a1);
-    intervals.push_back(a2);
-    intervals.push_back(a3);
-    intervals.push_back(a4);
-    intervals.push_back(b1);
-    intervals.push_back(b2);
+    switch (input)
+    {
+    case 1:
+        cout << "Losowy przyklad: (wylosowano liczbe zmiennych = " << n << ")\n";
 
-    hansen_intervals = hansen_parser::parse(intervals);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a1 = rand() % 100+1;
+                a2 = rand() % 100+1;
+                element = interval(min(a1, a2), max(a1, a2));
+                rows.push_back(element);
+            }
+            matA.push_back(rows);
+            rows.clear();
+        }
 
-    vector<vector<hansen_interval>> matA1 = { {hansen_intervals[0], hansen_intervals[1]}, {hansen_intervals[2], hansen_intervals[3]} };
-    vector<vector<hansen_interval>> matb1 = { {hansen_intervals[4]},{hansen_intervals[5]} };
-    hansen_interval_matrix A1 = hansen_interval_matrix(matA1);
-    hansen_interval_matrix B1 = hansen_interval_matrix(matb1);
+        for (int i = 0; i < n; i++) {
+            a1 = rand() % 100 + 1;
+            a2 = rand() % 100 + 1;
+            element = interval(min(a1, a2), max(a1, a2));
+            rows.push_back(element);
+            matB.push_back(rows);
+            rows.clear();
+        }
 
+        break;
 
-
-    vector<vector<interval>> matA = { {intervals[0], intervals[1]}, {intervals[2], intervals[3]} };
-    vector<vector<interval>> matb = { {intervals[4]},{intervals[5]} };
+    case 2:
+        cout << "Podaj liczbe zmiennych\n";
+        cin >> n;
+        cout << "Podaj kolejne wiersze macierzy A\n\tnp. 2;5;7;4 oznacza [(1,2), (7,4)]\n";
+        for (int i = 0; i < n; i++) {
+            cin >> row_input;
+            rows = split_row(row_input);
+            matA.push_back(rows);
+        }
+        cout << "Podaj kolejne wiersze macierzy b\n";
+        for (int i = 0; i < n; i++) {
+            cin >> row_input;
+            rows = split_row(row_input);
+            matB.push_back(rows);
+        }
+        break;
+    default:
+        cout << "Nie ma takiej opcji";
+        break;
+    }
     interval_matrix A = interval_matrix(matA);
-    interval_matrix b = interval_matrix(matb);
+    cout << "A:\n" << A;
+    interval_matrix b = interval_matrix(matB);
+    cout << "b:\n" << b;
 
 
     system_of_linear_equations sole = system_of_linear_equations(A, b);
-    vector<interval> I = solve(sole);
-
-    system_of_linear_equations_hansen_form sole_hansen = system_of_linear_equations_hansen_form(A1, B1);
-    vector<hansen_interval> I1 = solve_using_hansen(sole_hansen);
+    return (sole);
 }
-
 
 int main()
 {
-    //kod testowy
-    interval x, y;
-    std::vector <interval> intervals;
-    std::vector <hansen_interval> hansen_intervals;
-
-    x = interval(1, 2);
-    y = interval(3, 4);
-
-    intervals.push_back(x);
-    intervals.push_back(y);
-
-    hansen_intervals = hansen_parser::parse(intervals);
-    hansen_parser::print_intervals_information(intervals[0], hansen_intervals[0], "x");
-    hansen_parser::print_intervals_information(intervals[1], hansen_intervals[1], "y");
-    hansen_parser::print_intervals_information(intervals[0] + intervals[1], hansen_intervals[0] + hansen_intervals[1], "x + y");
-    hansen_parser::print_intervals_information(intervals[0] - intervals[1], hansen_intervals[0] - hansen_intervals[1], "x - y");
-    hansen_parser::print_intervals_information(intervals[0] * intervals[1], hansen_intervals[0] * hansen_intervals[1], "x * y");
-    hansen_parser::print_intervals_information(intervals[0] / intervals[1], hansen_intervals[0] / hansen_intervals[1], "x / y");
-
-    //testowanie macierzy i liniowego układu równań
-    std::vector<std::vector<interval>> matA = { {interval(2, 4), interval(-1, 1)}, {interval(-1, 1), interval(2, 4)} };
-    std::vector<std::vector<interval>> matb = { {interval(-3, 3)}, {interval(0)} };
-    interval_matrix A = interval_matrix(matA);
-    interval_matrix b = interval_matrix(matb);
-    system_of_linear_equations sole = system_of_linear_equations(A, b);
-    std::cout << sole << std::endl << std::endl;
-    std::cout << hansen_parser::parse_sole(sole) << std::endl;
+    srand(time(NULL));
 
 
-    run_example();
+    system_of_linear_equations sole = create_example();
+    vector<interval> I = solve(sole);
 
     return 0;
 }
